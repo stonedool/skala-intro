1383180c465a0ad02ed7b59547c72f8f7b0909f5
정찬혁 <stonedool@naver.com>
2025-12-29 16:33:41 +0900

    에코 프로그램 개선

--- echo.py
+++ echo.py
@@ -1,3 +1,8 @@
-sentence = input("문장을 입력하세요")
+while True:
+    sentence = input("문장을 입력하세요: ")
 
-print(sentence)
+    if sentence == "!quit":
+        print("프로그램을 종료합니다.")
+        break
+
+    print(sentence)
